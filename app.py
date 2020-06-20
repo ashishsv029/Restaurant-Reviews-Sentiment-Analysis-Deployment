08ea47f62dbb8f24cbb417ed0b06b5c6caa5e0c5
Nag Ashish S V <50989117+ashishsv029@users.noreply.github.com>
2020-06-21 02:35:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 filename = 'restaurant-sentiment-mnb-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 cv = pickle.load(open('cv-transform.pkl','rb'))
-from nltk.stem.porter import PorterStemmer
+#from nltk.stem.porter import PorterStemmer
 app = Flask(__name__)
 
 @app.route('/')
@@ -31,8 +31,8 @@ def predictor():
         review=re.sub('[^a-zA-z]',' ',st)
         review=review.lower()
         review=review.split()
-        ps=PorterStemmer()
-        review = [ps.stem(word) for word in review if not word in ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]]
+        #ps=PorterStemmer()
+        review = [word for word in review if not word in ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]]
         review=' '.join(review)
         corpus.append(review)
     trail_x=cv.transform(corpus).toarray()
